196266731b357bad4867a1153686c4c30b776fc1
Lesiio <37675386+Lesiio@users.noreply.github.com>
2018-03-23 05:22:12 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include<iostream>
+#include "LaborkaConfig.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+#else
+#include <cmath>
+#endif
 
 int main()
 {
-	int degree;
+	double degree;
 		std::cout<<"Podaj liczbe calkowita: ";
 		std::cin>>degree;
 		getchar();
 		
+	#ifdef USE_TRIGONOMETRY_DEGREE
 		double a = degreemath::sinus(degree);
 		double b = degreemath2::cosinus(degree);
 		double c = degreemath3::tangens(degree);
 		double d = degreemath4::cotangens(degree);
 		std::cout << "sinus" << a << " cosinus " << b << "tangens" << c << "cotangens" << d;
+		getchar();
+	#else
+		double a = std::sin(degree);
+		double b = std::cos(degree);
+		double c = std::tan(degree);
+		double d = 1/tan(degree);
+		std::cout << "biblioteka cmath: sinus" << a << " cosinus " << b << "tangens" << c << "cotangens" << d;
+		getchar();
+	#endif
 
 	return 0; 
 }
